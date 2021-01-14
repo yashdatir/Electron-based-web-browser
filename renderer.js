29176f41e6a90b61f0497f63cc15c48e3457b5e6
yashdatir <yashdatir@gmail.com>
2021-01-14 22:16:33 +0530

    Added React in dev

--- renderer.js
+++ renderer.js
@@ -1,6 +1,7 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+class App extends React.Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
